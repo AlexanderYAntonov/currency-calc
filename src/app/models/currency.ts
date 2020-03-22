f0bf2c84f08c3767bc57611844136660dc2de63f
Alexander Antonov <a.antonov76358@gmail.com>
2020-03-22 23:39:31 +0700

    исправил кавычки

--- src/app/models/currency.ts
+++ src/app/models/currency.ts
@@ -15,30 +15,30 @@ export interface PositionTotalPrice {
 export interface TotalCartPrice {
   rubles: number;
   euros: number;
-  "US dollars": number;
+  'US dollars': number;
   pounds: number;
   yens: number;
 }
 
 export const CURRENCY_CODES: CurrencyCode[] = [
   {
-    key: "rubles",
-    code: "RUB"
+    key: 'rubles',
+    code: 'RUB'
   },
   {
-    key: "euros",
-    code: "EUR"
+    key: 'euros',
+    code: 'EUR'
   },
   {
-    key: "US dollars",
-    code: "USD"
+    key: 'US dollars',
+    code: 'USD'
   },
   {
-    key: "pounds",
-    code: "GBP"
+    key: 'pounds',
+    code: 'GBP'
   },
   {
-    key: "yens",
-    code: "JPY"
+    key: 'yens',
+    code: 'JPY'
   }
 ];
